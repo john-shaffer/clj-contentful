79f18eef07f8e4a742c564e3a99c8b5582690091
John Shaffer <jshaffer2112@gmail.com>
2019-09-01 03:51:23 -0500

    Replace get with a cda-request function and add a cda-server constant.

--- src/clj_contentful.clj
+++ src/clj_contentful.clj
@@ -3,15 +3,23 @@
             [clj-http.client :as client]
             [cemerick.url :as url]))
 
+(def ^:const cda-server "https://cdn.contentful.com")
+
 (defn request [config f path]
   (let [{:keys [access-token server space-id]} config]
-    (->> {:headers {:Authorization (str "Bearer " access-token)}}
+    (->> {:headers {:Authorization (str "Bearer " access-token)}} ; OAuth 2.0
          (f (str (url/url server path)))
          :body
          json/parse-string)))
 
-(defn get [config path]
-  (request config client/get path))
+(defn cda-request
+  "Make a GET request to the Content Delivery API. Returns the response body
+  parsed as a map."
+  [config subpath]
+  (let [config (if (:server config)
+                 config
+                 (assoc config :server cda-server))]
+    (request config client/get (str "spaces/" (:space-id config) "/" subpath))))
 
 (defn space-entries [config]
-  (get config (str "spaces/" (:space-id config) "/entries")))
+  (cda-request config "entries"))
