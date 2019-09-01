e0a90a3633474b3ef668f8a6148f5ec7be0be4cc
John Shaffer <jshaffer2112@gmail.com>
2019-09-01 06:38:53 -0500

    Return keyword keys in response.

--- src/clj_contentful.clj
+++ src/clj_contentful.clj
@@ -7,10 +7,10 @@
 
 (defn request [config f path]
   (let [{:keys [access-token server space-id]} config]
-    (->> {:headers {:Authorization (str "Bearer " access-token)}} ; OAuth 2.0
-         (f (str (url/url server path)))
+    (-> (f (str (url/url server path))
+           {:headers {:Authorization (str "Bearer " access-token)}}) ; OAuth 2.0
          :body
-         json/parse-string)))
+         (json/parse-string true))))
 
 (defn cda-request
   "Make a GET request to the Content Delivery API. Returns the response body
@@ -20,6 +20,11 @@
                  config
                  (assoc config :server cda-server))]
     (request config client/get (str "spaces/" (:space-id config) "/" subpath))))
+
+(defn get-space
+  "Get the space referred to by config."
+  [config]
+  (cda-request config ""))
 
 (defn space-entries [config]
   (cda-request config "entries"))
