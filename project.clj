89cb0ab3b498d7cbda88c53572f6af8c8d598bef
John Shaffer <jshaffer2112@gmail.com>
2019-09-23 09:54:59 -0500

    Version 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-contentful "1.1.0-SNAPSHOT"
+(defproject clj-contentful "1.1.0"
   :description "A Clojure library for the Contentful APIs."
   :url "https://github.com/john-shaffer/clj-contentful"
   :license {:name "The MIT License"
